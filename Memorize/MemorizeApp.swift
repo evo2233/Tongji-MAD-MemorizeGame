cdf4219b1d20328fad4d568b5460204be2f4fa71
syr <13614356534@139.com>
2024-10-11 15:01:30 +0800

    Lecture2Finished

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by admin on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
