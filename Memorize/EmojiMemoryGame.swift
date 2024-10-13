9381e1b835663275a2e1fa35e4e7e653035a8db8
evo2233 <152169477+evo2233@users.noreply.github.com>
2024-10-13 15:15:46 +0800

    Update 可观察属性的注释

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ func createCardContent(forPairAtIndex index: Int) -> String {
     return ["😆","🥹","😅","🥲","😇","😉","🥰","😋"][index] // like array[index] return a emoji
 }*/
 
-class EmojiMemoryGame: ObservableObject {   // without ObservableObject change can't be print
+class EmojiMemoryGame: ObservableObject {   // ObservableObject是可观察类的标记，在Model中isFaceUp改变后自动通知View
     private static let emojis = ["😆","🥹","😅","🥲","😇","😉","🥰","😋"]
     // we can use EmojiMemoryGame.emojis anywhere without declare emojis global
     
@@ -28,7 +28,7 @@ class EmojiMemoryGame: ObservableObject {   // without ObservableObject change c
     // cardContentFactory: {(index: Int) -> String in    --> because of type inference we can abondon Int & String
     // numberOfPairsOfCards: 4, cardContentFactory: {index in...})    --> we konw this func have only 2 parameters only mark 1 is OK
     
-    @Published private var model = createMemoryGame()
+    @Published private var model = createMemoryGame()    // @Publish专门用于标记需要被观察的属性
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
@@ -38,7 +38,7 @@ class EmojiMemoryGame: ObservableObject {   // without ObservableObject change c
     
     func shuffle() {
         model.shuffle()
-        objectWillChange.send() // without it change can't be print
+        objectWillChange.send() // 手动调用objectWillChange可以让你控制何时通知视图更新
     }
     
     func choose(_ card: MemoryGame<String>.Card) {  // _ means parameter has no external name
